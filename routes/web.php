440be627619addecbc20b5828bdb37248c97400a
lucas3d4783 <lucas3d4783@gmail.com>
2020-12-23 01:01:25 -0300

    Aula 10 - rotas (any, match, get, put , ... )

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::any('any', function (){
+    return 'Any -> aceita qualquer método HTTP';
+});
+
+Route::match(['get', 'post'], 'match', function (){
+    return 'Match -> você especifica em um array os tipos de métodos HTTP aceitos para essa rota';
+});
+
+Route::get('contato', function () {
+    return view('contact');
+});
+
+
 Route::get('/', function () {
     return view('welcome');
 });

50075a787b471123d42afaf2f6e706334d38a5bd
lucas3d4783 <lucas3d4783@gmail.com>
2020-12-23 01:11:52 -0300

    Aula 11 - rotas - trabalhando com parâmetros

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('produtos/{idProduct?}', function($idProduct = ''){ //colocando o "?" ele informa que o parâmetro é opcional e definindo um calor default vazio
+    return "Produto(s) {$idProduct}";
+});
+
+Route::get('categoria/{flag}/posts', function($flag){
+    return "Posts da categoria: {$flag}";
+});
+
+Route::get('categorias/{flag}', function($flag){
+    return "Produtos da categoria: {$flag}";
+});
+
 Route::any('any', function (){
-    return 'Any -> aceita qualquer método HTTP';
+    return 'Any -> aceita qualquer método HTTP -> não é recomendado pois pode deixar vulnerável';
 });
 
 Route::match(['get', 'post'], 'match', function (){

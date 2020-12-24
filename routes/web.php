d3cf0042f5eaea7f7b710bae747be77c9c094e02
lucas3d4783 <lucas3d4783@gmail.com>
2020-12-24 01:51:34 -0300

    Aula 13 - Rotas nomeadas no Laravel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('redirect3', function (){
+    return redirect()->route('url.name');
+});
+
+Route::get('/name-url', function () {
+    return 'Hey hey hey';
+})->name('url.name');
+
 Route::view('view', 'welcome');
 
 /*

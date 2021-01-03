9ef27c1f54cc9eb1f2fcee1ba5c2735b831c4e4c
lucas3d4783 <lucas3d4783@gmail.com>
2021-01-03 06:29:34 -0300

    Aula 22 - Introdução a views

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,11 +6,29 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index(){
-        $products = ['p1', 'p2', 'p3'];
-        return $products;
-        //return 'listagem de produtos';
+
+    public function __construct (){
+        //$this->middleware('auth'); //definindo um middleware para todos os métodos do controller
+        /*$this->middleware('auth')->only([ //especificando os métodos onde o middleware será aplicado
+            'create', 
+            'storage',
+            'destroy',
+        ]);*/
+        $this->middleware('auth')->except([ // especificando os métodos aos quais não será aplicado o middleware
+            'index',
+        ]);
     }
+
+    // public function index(){
+    //     $products = ['p1', 'p2', 'p3'];
+    //     return $products;
+    //     //return 'listagem de produtos';
+    // }
+    public function index(){
+        $context['teste'] = 'teste';
+        return view('teste', $context);
+    }
+
     public function show($id){
         return "Exibindo produto de ID: {$id}";
     }
